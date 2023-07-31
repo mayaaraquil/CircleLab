f1efdbe0cb7915b414c253ae61766d1e5a78510f
Maya Araquil <mayaaraquil775@gmail.com>
2023-07-31 11:36:13 -0400

    CircleLab

---
+++ source/repos/Circles Lab/Circles Lab/Program.cs
@@ -0,0 +1,32 @@
+﻿
+static void Main(string[] args)
+{
+    Console.WriteLine("Enter the radius of the circle (a number with decimals please!): ");
+    double radius;
+    while (!double.TryParse(Console.ReadLine(), out radius))
+    {
+        Console.WriteLine("oops, Invalid input. Please enter a valid radius:");
+    }
+
+    circle circle = new Circle(radius);
+
+    Console.WriteLine("Diameter: " + circle.CalculateDiameter());
+    Console.WriteLine("Circumference: " circle.CalculateCircumference());
+    Console.WriteLine("Area: " + circle.CalculateArea());
+
+    Console.WriteLine("Do you want the circle to grow? (yes/no)");
+    string userInput = Console.ReadLine().ToLower();
+
+    while (userInput == "yes")
+    {
+        circle.Grow();
+
+        Console.WriteLine("Diameter: " + circle.CalculateDiameter());
+        Console.WriteLine("Circumference: " + circle.CalculateCircumference());
+        Console.WriteLine("Area: " + circle.CalculateArea());
+
+        Console.WriteLine("Do you want the circle to grow? (yes/no)");
+        userInput = Console.ReadLine().ToLower();
+    }
+    Console.WriteLine("okay! bye bye for now, The final radius of the circle is: " circle.Getradius());
+}
